9c44d9cfe2cc0336e0e737990fc4608789f59e9a
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-24 01:53:07 +0530

    DSA

---
+++ Array_Module/1.1.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i =0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int max = arr[0];
+	printf("[");
+	for(int i=0;i<n;i++){
+		if(i<n-1){
+			printf("%d, ",arr[i]);
+		}
+		else
+			printf("%d",arr[i]);
+	}
+	printf("]");
+	for(int i = 1;i<n;i++){
+		if(arr[i]>max){
+			max = arr[i];}
+		}
+	printf("\n");
+	printf("%d",max);
+}
