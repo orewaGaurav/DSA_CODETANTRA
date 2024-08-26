654f921cece595c08a6975ef255e326c922225ca
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-26 11:06:23 +0530

    DSA

--- Array_Module/1.1.2.c
+++ Array_Module/1.1.2.c
@@ -1,3 +1,32 @@
+// Implement a program to find the sum of all elements in an array of size 
+// n
+// .
+
+
+
+// Input Format:
+
+// The first line of the input represents an integer 
+// n
+// The second line of the input represents 
+// n
+//  space separated elements of the array (Integers)
+// Output Format:
+
+// The first line of the output represents the input array.
+// The second line of the output contains an integer representing the sum of all element of the array.
+
+// Test case 1
+// 3	
+// 1 2 3	
+// [1,·2,·3]⏎	
+// 6	
+// Test case 2
+// 5	
+// 4 -2 7 1 0	
+// [4,·-2,·7,·1,·0]⏎	
+// 10
+
 #include <stdio.h>
 int main(){
 	int n;
