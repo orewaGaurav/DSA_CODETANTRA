c8a76a6b7c03ab87ff32a8e7ad368c2c9cd9524e
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-26 11:02:04 +0530

    DSA

--- Array_Module/1.1.6.c
+++ Array_Module/1.1.6.c
@@ -1,3 +1,51 @@
+// Implement a program that takes the number of rows 
+// r
+//  and columns 
+// c
+//  from the user to create a 2D array, and then traverses and prints the array in both row-major and column-major order.
+
+
+
+// Input Format:
+
+// The first line of input contains two integers 
+// r
+//  and 
+// c
+//  separated by a space, representing the number of rows and columns, respectively.
+// The second line of input contains 
+// r×c
+//  space-separated elements to populate the 2D array.
+
+// Output Format:
+
+// The first 
+// r
+//  lines of the output should each contain 
+// c
+//  space-separated elements, representing the row-major order traversal of the array.
+// The next 
+// c
+//  lines of the output should each contain 
+// r
+//  space-separated elements, representing the column-major order traversal of the array.
+
+// Test case 1
+// 2 3	
+// 1 2 3 4 5 6	
+// 1·2·3·⏎	
+// 4·5·6·⏎	
+// 1·4·⏎	
+// 2·5·⏎	
+// 3·6·	
+// Test case 2
+// 2 2	
+// 1 3 2 4	
+// 1·3·⏎	
+// 2·4·⏎	
+// 1·2·⏎	
+// 3·4·
+
 #include <stdio.h>
 int main(){
 	int r,c;
