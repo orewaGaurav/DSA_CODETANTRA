4f94b745b749c2a49f0c85f367dc53f4bf5775ba
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-26 10:58:53 +0530

    DSA

--- Array_Module/1.1.8.c
+++ Array_Module/1.1.8.c
@@ -1,3 +1,35 @@
+// Implement a program that determines whether a given matrix is sparse and, if so, prints its non-zero elements. A matrix is considered sparse if the number of zero elements is greater than the number of non-zero elements.
+
+
+
+// Input Format:
+
+// The first line of input contains two integers r and c
+// separated by a space, representing the number of rows and columns, respectively.
+// The next r lines each contain c
+// space-separated integers, representing the elements of the matrix.
+
+
+// Output Format:
+
+// The first line of the output should be a boolean value (True or False), indicating whether the matrix is sparse.
+// If the matrix is sparse, print the non-zero elements in the format row column value, each on a new line. If the matrix is not sparse, print nothing.
+
+// Test case 1
+// 3 3	
+// 0 0 3	
+// 0 0 6	
+// 0 0 0	
+// True⏎	
+// 0·2·3⏎	
+// 1·2·6	
+// Test case 2
+// 3 3	
+// 1 2 3	
+// 4 0 6	
+// 7 8 9	
+// False
+
 #include <stdio.h>
 int main(){
 	int r,c,i,j;
