d04c7557df045bcc89bc45401802683ec61552de
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-26 10:55:36 +0530

    DSA

--- Array_Module/1.1.10.c
+++ Array_Module/1.1.10.c
@@ -1,3 +1,34 @@
+// Write a program to check whether the given element is present or not in the array of elements using linear search.
+
+
+
+// Input format:
+
+// The first line of input contains an integer representing the no. of elements of the array.
+// The second line of input contains the array of integers separated by space.
+// The last line of input contains the key element to be searched.
+
+
+// Output format:
+
+// If the element is found, print the index.
+// If the element is not found, print Not found.
+
+
+// Sample Test Case:
+
+// Input:
+
+// 7
+
+// 1 2 3 4 3 5 6
+
+// 3
+
+// Output:
+
+// 2
+
 #include <stdio.h>
 int main(){
 	int n;
